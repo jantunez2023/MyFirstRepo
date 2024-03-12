ae765330ce67d02848432107e7df57803b648efa
jantunez2023 <146232695+jantunez2023@users.noreply.github.com>
2024-03-11 21:28:50 -0600

    Ex5_1 Fileupload
    Python file Ex5_1

---
+++ Ex5_1.py
@@ -0,0 +1,29 @@
+#Printing out Total, count and avergae
+
+#Initializig variables
+suma = 0
+contador = 0
+
+user_data = input ("Enter a number:")
+if user_data == "done" :
+    print ("Total: ", suma, " Counter: ", contador)
+
+else :
+    while user_data != "done" :
+
+        try :
+             user_data = float (user_data)
+             contador = contador + 1
+             suma = suma + user_data
+             user_data = input ("Enter a number:")
+        except:
+             print ("Invalid input. Number was expected: ", user_data)
+             user_data = input ("Enter a number:")
+             if user_data == "done" :
+                 if contador == 0:
+                    print ("Total: ", suma, " Counter: ", contador)
+                    quit()
+                 else :
+                     print ("Total: ", suma, " Counter: ", contador, "Average:", suma/contador)
+                     quit()
+    print ("Total: ", suma, " Counter: ", contador, " Average:", suma/contador)
